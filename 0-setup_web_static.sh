dad953c5ae2dd0bfe3da943cf7308592bd15663d
Josph Simon <kakabrather@gmail.com>
2023-04-09 21:57:23 +0300

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+
+# Install nginx if it is not already installed
+if [ ! -x "$(command -v nginx)" ]; then
+  sudo apt-get update
+  sudo apt-get -y install nginx
+fi
+
+# Create required directories if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo chown -R ubuntu:ubuntu /data/
+
+# Create a fake HTML file to test nginx configuration
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create symbolic link to test folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-conf="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-sudo sed -i "45i $conf" /etc/nginx/sites-available/default
-sudo service nginx start
+
+# Update nginx configuration to serve the content of /data/web_static/current/
+# Restart nginx after updating configuration
+sudo sed -i '29i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
-sudo service nginx reload
+
+exit 0
